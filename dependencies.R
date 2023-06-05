7e6e38ea883417d8f211fbae4b29596aaf59ee4d
Pawan Rama Mali <prm@outlook.in>
2023-06-05 22:11:40 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,2 +1,6 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(rhino)
+library(shinyjs)
+library(shinyalert)
+library(tidyverse)
+library(shinythemes)
